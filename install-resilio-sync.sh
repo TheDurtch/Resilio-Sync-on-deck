2def0d5902314ef79e287d8e0c93d7d90861d5b0
Kate <thedurtch@gmail.com>
2022-09-14 03:29:12 -0500

    Updated script and changed tar.xz

--- install-resilio-sync.sh
+++ install-resilio-sync.sh
@@ -1,1 +1,7 @@
- 
+#!/bin/bas
+mkdir -p ~/bin
+mkdir -p ~/.config/resilio-sync/storage/
+#cd ~/
+wget https://git.fzbu.org/YandereKate/Resilio-Sync/raw/branch/master/Resilio-Sync.tar.xz
+tar -xJvpf Resilio-Sync.tar.xz
+rm Resilio-Sync.tar.xz
