6dbfd19a81c7fef618c8f08a6b37c2bce409ce1d
YandereKate <yanderekate@noreply.fzbu.org>
2022-10-02 01:43:17 +0000

    Update 'install-resilio-sync.sh'
    oops

--- install-resilio-sync.sh
+++ install-resilio-sync.sh
@@ -5,7 +5,7 @@ mkdir -p /tmp/RESILIO-TMP
 cd ~/
 wget https://git.fzbu.org/YandereKate/Resilio-Sync/raw/branch/master/Resilio-Sync.tar.xz
 tar -xJvpf Resilio-Sync.tar.xz
-cd ~/tmp/RESILIO-TMP
+cd /tmp/RESILIO-TMP
 wget -O resilio-sync.rpm https://download-cdn.resilio.com/2.7.3.1381/RPM/resilio-sync-2.7.3.1381-1.x86_64.rpm
 ark -b resilio-sync.rpm
 mv /tmp/RESILIO-TMP/usr/bin/rslsync ~/bin/
